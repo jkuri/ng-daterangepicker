fd970fccaa34bda13fcdbc8bc4acf8e53d6d4070
Jan Kuri <jkuri88@gmail.com>
2017-03-08 11:00:14 +0100

    feat(range): range selection (predefined)

--- src/app/components/angular-paypal-datepicker/angular-paypal-datepicker.component.ts
+++ src/app/components/angular-paypal-datepicker/angular-paypal-datepicker.component.ts
@@ -13,14 +13,14 @@ export class AngularPayPalDatepickerComponent implements OnInit {
   dateTo: Date;
   dayNames: string[];
   days: any[];
+  range: 'tm' | 'lm' | 'lw' | 'tw' | 'ty' | 'ly';
 
   constructor(private elementRef: ElementRef) { }
 
   ngOnInit() {
     this.opened = false;
     this.date = dateFns.startOfDay(new Date());
-    this.dateFrom = dateFns.startOfDay(dateFns.subWeeks(this.date, 3));
-    this.dateTo = this.date;
+    this.selectRange('tw');
     this.initNames();
     this.generateCalendar();
   }
@@ -85,6 +85,7 @@ export class AngularPayPalDatepickerComponent implements OnInit {
   }
 
   selectDate(e: MouseEvent, index: number): void {
+    e.preventDefault();
     let selectedDate: Date = this.days[index].date;
     if ((this.opened === 'from' && dateFns.isAfter(selectedDate, this.dateTo)) ||
         (this.opened === 'to' && dateFns.isBefore(selectedDate, this.dateFrom))) {
@@ -109,6 +110,43 @@ export class AngularPayPalDatepickerComponent implements OnInit {
 
   nextMonth(): void {
     this.date = dateFns.addMonths(this.date, 1);
+    this.generateCalendar();
+  }
+
+  selectRange(range: 'tm' | 'lm' | 'lw' | 'tw' | 'ty' | 'ly'): void {
+    let today = dateFns.startOfDay(new Date());
+
+    switch (range) {
+      case 'tm':
+        this.dateFrom = dateFns.startOfMonth(today);
+        this.dateTo = dateFns.endOfMonth(today);
+        break;
+      case 'lm':
+        today = dateFns.subMonths(today, 1);
+        this.dateFrom = dateFns.startOfMonth(today);
+        this.dateTo = dateFns.endOfMonth(today);
+        break;
+      case 'lw':
+        today = dateFns.subWeeks(today, 1);
+        this.dateFrom = dateFns.startOfWeek(today);
+        this.dateTo = dateFns.endOfWeek(today);
+        break;
+      case 'tw':
+        this.dateFrom = dateFns.startOfWeek(today);
+        this.dateTo = dateFns.endOfWeek(today);
+        break;
+      case 'ty':
+        this.dateFrom = dateFns.startOfYear(today);
+        this.dateTo = dateFns.endOfYear(today);
+        break;
+      case 'ly':
+        today = dateFns.subYears(today, 1);
+        this.dateFrom = dateFns.startOfYear(today);
+        this.dateTo = dateFns.endOfYear(today);
+        break;
+    }
+
+    this.range = range;
     this.generateCalendar();
   }
 

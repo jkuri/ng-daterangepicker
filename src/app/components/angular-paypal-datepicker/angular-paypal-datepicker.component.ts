5cb37c5846c3116e963e469164f291a92fa0281a
Jan Kuri <jkuri88@gmail.com>
2017-03-08 08:45:03 +0100

    feat(cal): date selection, blur click

--- src/app/components/angular-paypal-datepicker/angular-paypal-datepicker.component.ts
+++ src/app/components/angular-paypal-datepicker/angular-paypal-datepicker.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 import * as dateFns from 'date-fns';
 
 @Component({
@@ -7,25 +7,29 @@ import * as dateFns from 'date-fns';
   styleUrls: ['angular-paypal-datepicker.sass']
 })
 export class AngularPayPalDatepickerComponent implements OnInit {
+  opened: false | 'from' | 'to';
   date: Date;
   dateFrom: Date;
   dateTo: Date;
   dayNames: string[];
   days: any[];
 
+  constructor(private elementRef: ElementRef) { }
+
   ngOnInit() {
+    this.opened = false;
     this.date = dateFns.startOfDay(new Date());
     this.dateFrom = dateFns.startOfDay(dateFns.subWeeks(this.date, 3));
     this.dateTo = this.date;
     this.initNames();
-    this.generateDays();
+    this.generateCalendar();
   }
 
   initNames(): void {
     this.dayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
   }
 
-  generateDays(): void {
+  generateCalendar(): void {
     this.days = [];
     let start: Date = dateFns.startOfMonth(this.date);
     let end: Date = dateFns.endOfMonth(this.date);
@@ -68,18 +72,29 @@ export class AngularPayPalDatepickerComponent implements OnInit {
     this.days = prevMonthDays.concat(days);
   }
 
+  toggleCalendar(e: MouseEvent, selection: 'from' | 'to'): void {
+    this.opened = this.opened ? false : selection;
+  }
+
   selectDate(e: MouseEvent, index: number): void {
     this.dateTo = this.days[index].date;
-    this.generateDays();
+    this.generateCalendar();
   }
 
   prevMonth(): void {
     this.date = dateFns.subMonths(this.date, 1);
-    this.generateDays();
+    this.generateCalendar();
   }
 
   nextMonth(): void {
     this.date = dateFns.addMonths(this.date, 1);
-    this.generateDays();
+    this.generateCalendar();
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleBlurClick(e: MouseEvent) {
+    if (!this.elementRef.nativeElement.contains(e.target) && !e.srcElement.classList.contains('day-num')) {
+      this.opened = false;
+    }
   }
 }

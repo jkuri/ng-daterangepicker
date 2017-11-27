7ccdc5f8d765eb115fd62420932e571635e0a626
Marcel Kilian <marce.kilian@kiwigrid.com>
2017-11-27 16:46:41 +0100

    setup angular2 yeoman build

--- src/main.aot.ts
+++
@@ -1,9 +0,0 @@
-import './polyfills';
-import './styles';
-
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './ngfactory/src/app/app.module.ngfactory';
-
-enableProdMode();
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);

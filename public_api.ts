9b1d453f5bdcf02f90e48f7b691a75c57a982bfd
Jan Kuri <jkuri88@gmail.com>
2018-03-22 22:36:44 +0100

    build(ng-packagr): integrate ng-packagr

---
+++ public_api.ts
@@ -0,0 +1,2 @@
+export * from './src/app/ng-daterangepicker/ng-daterangepicker.module';
+export * from './src/app/ng-daterangepicker/ng-daterangepicker.component';
---
+++ src/app/ng-daterangepicker/ng-daterangepicker.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { NgDateRangePickerComponent } from './ng-daterangepicker.component';
+
+@NgModule({
+  declarations: [NgDateRangePickerComponent],
+  exports: [NgDateRangePickerComponent]
+})
+export class NgDateRangePickerModule { }
